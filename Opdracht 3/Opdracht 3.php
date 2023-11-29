e74be07f669fcbd075a756127992a894bf7c5297
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2023-11-29 10:06:48 +0100

    Opdracht 3.php done

---
+++ Opdracht 3/Opdracht 3.php
@@ -0,0 +1,15 @@
+<?php
+    // Opdracht 3.php
+    // Author = Dylan van Schouwen
+    $string1 = "Hello";
+    $string2 = "World!";
+
+    $number1 = 5;
+    $number2 = 10;
+
+    echo "<p>$string1 $string2</p>";
+
+    print "<p>Number 1: $number1</p>";
+    print "<p>Number 2: $number2</p>";
+    
+?>
