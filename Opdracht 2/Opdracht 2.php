e30994bd272799a4233a8d5a2504261521276f58
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2023-11-29 10:03:40 +0100

    Opdracht 2.php done

--- Opdracht 2/Opdracht 2.php
+++ Opdracht 2/Opdracht 2.php
@@ -10,9 +10,23 @@ echo "$test123";
 echo "<h1>Hallo wereld,</h1>";
 echo "Ik zit op het ";
 // Voorbeeld van een variabele in PHP.
-$schoolnaam = "Techniek College Rotterdam !";
+$schoolnaam = "Techniek College Rotterdam! <br> <br>";
 
 // Met echo kun je iets op het scherm tonen.
 echo "$schoolnaam";
+
+date_default_timezone_set ("Europe/Amsterdam");
+$today = date ("j F Y");
+echo "vandaag is het: $today <br>";
+
+$time = date("H:i");
+echo "het is nu: $time uur <br>";
+
+$month = date ("F");
+$daysinmonth = date ("t");
+echo "deze maand, $month heeft $daysinmonth dagen <br>";
+
+$week = date ("W");
+echo "deze week is het week: $week";
 
 ?>
