8d6c08995631bd6efe3b9611b84440e6404ee2da
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2023-11-29 11:52:47 +0100

    Opdracht 4.6 Done

---
+++ Opdracht 4/Opdracht 4.6.php
@@ -0,0 +1,24 @@
+<?php 
+// Opdracht 4.6.php
+// Author = Dylan van Schouwen
+
+date_default_timezone_set ("Europe/Amsterdam");
+
+$uur = date ('G');
+$min = date ('i'); 
+$temperatuur = 1;
+$vochtigheid = 100;
+
+echo "$uur:$min <br>";
+echo "$temperatuur <br>";
+echo "$vochtigheid <br>";
+
+if ($uur >= 8 && $uur <= 17){
+    echo "het is $uur:$min <br> de airco staat aan";
+} elseif ($temperatuur <= 20 && $vochtigheid <= 85){
+    echo "het is $temperatuur graden en de vochtigheid is $vochtigheid <br> de airco staat aan";
+} else{
+    Echo "de airco staat uit";
+}
+
+?>
