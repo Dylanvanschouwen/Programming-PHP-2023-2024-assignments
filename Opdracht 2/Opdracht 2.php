79c5dfd1e9d24a6e5bb912a91890fd386c5e3bd2
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2023-11-29 09:53:03 +0100

    Opdracht 2.php done

---
+++ Opdracht 2/Opdracht 2.php
@@ -0,0 +1,18 @@
+<?php
+// Opdracht 2.php
+// author: Dylan van Schouwen
+
+// test 123
+$test123 = "BOO";
+echo "$test123";
+
+//  Een voorbeeld van een echo en een print
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op het ";
+// Voorbeeld van een variabele in PHP.
+$schoolnaam = "Techniek College Rotterdam !";
+
+// Met echo kun je iets op het scherm tonen.
+echo "$schoolnaam";
+
+?>
